e5c82afdc3df6061e12f38bbb933c117a1104edb
Paul Miller <paulmiller@Pauls-MBP.fios-router.home>
2019-09-08 09:46:45 -0400

    Committing before location change

--- Gemfile
+++ Gemfile
@@ -1,10 +1,10 @@
 source 'https://rubygems.org'
 
-ruby "2.6.4"
+# ruby "2.6.4"
 
-gem 'rack', '~>2.0.7'
-gem 'vienna', '~> 0.4.2'
+# gem 'rack', '~>2.0.7'
+# gem 'vienna', '~> 0.4.2'
 
 # If using laptop, disable Ruby ver and enable these
-# gem 'rack', '~>1.6.11'
-# gem 'vienna'
+gem 'rack', '~>1.6.11'
+gem 'vienna'
--- public/js/main.js
+++ public/js/main.js
@@ -186,56 +186,56 @@ $(document).ready(function() {
 
   var data = [
     {
-      year: '2007-2008',
+      // year: '2007-2008',
       goals: '65',
       games: '82',
       avg: '0.79'
       
     },
     {
-      year: '2008-2009',
+      // year: '2008-2009',
       goals: '56',
       games: '79',
       avg: '0.7'
       
     },
     {
-      year: '2009-2010',
+      // year: '2009-2010',
       goals: '50',
       games: '72',
       avg: '0.69'
       
     },
     {
-      year: '2010-2011',
+      // year: '2010-2011',
       goals: '32',
       games: '79',
       avg: '0.40'
       
     },
     {
-      year: '2011-2012',
+      // year: '2011-2012',
       goals: '38',
       games: '78',
       avg: '0.48'
       
     },
     {
-      year: '2012-2013',
+      // year: '2012-2013',
       goals: '32',
       games: '48',
       avg: '0.66'
       
     },
     {
-      year: '2013-2014',
+      // year: '2013-2014',
       goals: '51',
       games: '78',
       avg: '0.65'
       
     },
     {
-      year: '2014-2015',
+      // year: '2014-2015',
       goals: '50',
       games: '76',
       avg: '0.66'
